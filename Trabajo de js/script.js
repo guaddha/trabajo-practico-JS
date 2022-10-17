2c1cef1e9e34d5fdbf30b519799d617e571045ca
guaddha <101990480+guaddha@users.noreply.github.com>
2022-10-17 16:55:38 -0300

    Add files via upload

---
+++ Trabajo de js/script.js
@@ -0,0 +1,18 @@
+var array= ["Guada" , "17" , "Widi el mejor profe" ];
+console.log (array);
+
+/#region suma
+let  num1= 20;
+let  num2= 21;
+function suma (num1, num2){
+    suma= num1 +num2;
+}
+console.log (suma);
+
+/#region resta
+let  num1= 10;
+let  num2= 21;
+function resta (num1, num2){
+    resta = num1 - num2;
+}
+console.log (resta);
